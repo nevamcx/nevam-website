4f03d05c007664908c449f1359e2cd633015f5a0
Brett Spradbury <brett.spradbury@nevamcx.com>
2024-02-27 15:30:39 -0300

    updates

--- nuxt.config.js
+++ nuxt.config.js
@@ -81,7 +81,7 @@ export default {
 
   hooks: {
     'build:manifest': (manifest) => {
-      const css = manifest['node_modules/nuxt/dist/app/entry.js'].css
+      const css = manifest['node_modules/nuxt/dist/app/entry.js']?.css
       if (css) {
         for (let i = css.length - 1; i >= 0; i--) {
           if (css[i].startsWith('entry')) css.splice(i, 1)
